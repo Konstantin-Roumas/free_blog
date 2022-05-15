d74d21067c982e7f908a38b54e27b7e5e580c436
kostya <konstantinroumas@gmail.com>
2022-05-16 01:12:15 +0300

    test

---
+++ spec/controllers/article_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :controller do
+  context "GET articles#index" do
+    it "show us success" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+  context "GET #show" do
+    let(:article){Article.create(title:"zxc", body:"zxc", tags:"zxc")}
+    it "returns a succes response" do
+      #/articles/{article-id}
+      get :show, params: {id: article}
+      expect(response.status).to eq(200)
+    end
+  end
+end
